96a3b72cc115304062b39b5464294b9743e862a8
Unknown <mallebedev@gmail.com>
2018-03-02 01:44:02 +0300

    Repair init.sh to start nginx. All working but path - kush
    Path of root in /etc/nginx/nginx.conf - Kush, to work change to name of PC(box)

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
+sudo /etc/init.d/nginx restart
 sudo nginx -s reload
-#sudo /etc/init.d/nginx restart
 
 #sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 #sudo /etc/init.d/nginx restart
